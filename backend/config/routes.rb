c1b266029865ed9bce3ab555e1847d3c2ddb8493
kybrdbnd <pranavpuri.p@hotmail.com>
2018-07-04 12:13:40 +0530

    jwt added

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :users
   resources :user_assets
   resources :users
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :company_assets
+  post 'auth_user' => 'authentication#authenticate_user'
+  get 'home' => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
